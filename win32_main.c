ebd49f1f1f58f356babcfec22b1c9f3df92d9635
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-05-23 21:58:03 +0300

    win32 init

---
+++ win32_main.c
@@ -0,0 +1,70 @@
+/* win32_main.cpp */
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+typedef int bool;
+#define true  1
+#define false 0
+
+static bool quit = false;
+static int exit_code = 0;
+
+// @TODO: basic drawing & msg loop, quit on esc
+
+LRESULT win32_window_proc(HWND h_wnd,
+                          UINT u_msg,
+                          WPARAM w_param,
+                          LPARAM l_param)
+{
+    return DefWindowProc(h_wnd, u_msg, w_param, l_param);
+}
+
+int APIENTRY WinMain(HINSTANCE h_inst, 
+                     HINSTANCE h_inst_prev, 
+                     PSTR cmdline, 
+                     int cmdshow)
+{
+    const char app_name[] = "My engine";
+
+    WNDCLASS wc = {0};
+    wc.style         = CS_HREDRAW;
+    wc.lpfnWndProc   = &win32_window_proc;
+    wc.hInstance     = h_inst;
+    wc.lpszClassName = app_name;
+    RegisterClass(&wc);
+
+    HWND window_handle = CreateWindowEx(0,
+                                        app_name,
+                                        app_name,
+                                        WS_EX_OVERLAPPEDWINDOW,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        NULL,
+                                        NULL,
+                                        h_inst,
+                                        NULL);
+    if (!window_handle) {
+        // @TODO: log
+        return 1;
+    }
+
+    ShowWindow(window_handle, cmdshow);
+
+    for (;;) {
+        MSG msg;
+        int msg_res;
+        while ((msg_res = GetMessage(&msg, window_handle, 0, 0)) != 0) {
+            if (msg_res == -1) {
+                // @TODO: log/deal with
+                return 2;
+            } else {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+    }
+
+    return 0;
+}
