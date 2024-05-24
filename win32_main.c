f28830cf798dc049ca1cc6686e8b19cdbae22352
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-05-24 12:00:28 +0300

    Made basic message loop & bg drawing

--- win32_main.c
+++ win32_main.c
@@ -16,7 +16,24 @@ LRESULT win32_window_proc(HWND h_wnd,
                           WPARAM w_param,
                           LPARAM l_param)
 {
-    return DefWindowProc(h_wnd, u_msg, w_param, l_param);
+    switch (u_msg) {
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        BeginPaint(h_wnd, &ps);
+        FillRect(ps.hdc, &ps.rcPaint, (HBRUSH)COLOR_WINDOW);
+        EndPaint(h_wnd, &ps);
+    } return 0;
+
+    case WM_KEYDOWN:
+        if (w_param == VK_ESCAPE) {
+            quit = true;
+            exit_code = 0;
+        }
+        return 0;
+    
+    default:
+        return DefWindowProc(h_wnd, u_msg, w_param, l_param);
+    }
 }
 
 int APIENTRY WinMain(HINSTANCE h_inst, 
@@ -27,35 +44,38 @@ int APIENTRY WinMain(HINSTANCE h_inst,
     const char app_name[] = "My engine";
 
     WNDCLASS wc = {0};
-    wc.style         = CS_HREDRAW;
+    wc.style         = 0;
     wc.lpfnWndProc   = &win32_window_proc;
     wc.hInstance     = h_inst;
     wc.lpszClassName = app_name;
     RegisterClass(&wc);
 
-    HWND window_handle = CreateWindowEx(0,
-                                        app_name,
-                                        app_name,
-                                        WS_EX_OVERLAPPEDWINDOW,
-                                        CW_USEDEFAULT,
-                                        CW_USEDEFAULT,
-                                        CW_USEDEFAULT,
-                                        CW_USEDEFAULT,
-                                        NULL,
-                                        NULL,
-                                        h_inst,
-                                        NULL);
+    HWND window_handle =
+        CreateWindowEx(0,
+                       app_name,
+                       app_name,
+                       WS_EX_OVERLAPPEDWINDOW | WS_EX_APPWINDOW,
+                       CW_USEDEFAULT,
+                       CW_USEDEFAULT,
+                       CW_USEDEFAULT,
+                       CW_USEDEFAULT,
+                       NULL,
+                       NULL,
+                       h_inst,
+                       NULL);
+
     if (!window_handle) {
         // @TODO: log
         return 1;
     }
 
     ShowWindow(window_handle, cmdshow);
+    UpdateWindow(window_handle);
 
     for (;;) {
         MSG msg;
         int msg_res;
-        while ((msg_res = GetMessage(&msg, window_handle, 0, 0)) != 0) {
+        while ((msg_res = PeekMessage(&msg, window_handle, 0, 0, PM_REMOVE)) != 0) {
             if (msg_res == -1) {
                 // @TODO: log/deal with
                 return 2;
@@ -64,7 +84,10 @@ int APIENTRY WinMain(HINSTANCE h_inst,
                 DispatchMessage(&msg);
             }
         }
+
+        if (quit)
+            break;
     }
 
-    return 0;
+    return exit_code;
 }
